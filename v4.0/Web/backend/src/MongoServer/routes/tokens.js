fd8d86146530fd374ffadb93b4f54e03f87b250c
eladdmti100 <eladdamti100@gmail.com>
2025-11-12 14:16:58 +0200

    Remove duplicate v4.0 folder after setting project root

--- v4.0/Web/backend/src/MongoServer/routes/tokens.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tokensController = require('../controllers/tokens');
-
-// POST /api/tokens for user login
-router.post('/', tokensController.authentication);
-
-module.exports = router;
--- v4.0/android/app/src/main/java/com/example/androidchillflix/main/models/LoginResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.androidchillflix.main.models;
-public class LoginResponse {
-    private String token;
-
-    public String getToken() {
-        return token;
-    }
-}
